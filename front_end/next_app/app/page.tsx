89bfc50ca1be2fb0e96e53b765cdb0595dd4858a
Harken <antarcticbearhkl@gmail.com>
2025-04-25 11:36:32 +1000

    pusha

--- front_end/next_app/app/page.tsx
+++ front_end/next_app/app/page.tsx
@@ -3,7 +3,7 @@ import FullPageSlider from "../components/fp";
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-      Hello world
+      <FullPageSlider />
     </div>
   );
 }
