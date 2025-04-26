b12591785ed1b1057de63b6b51005ce946308515
Harken <antarcticbearhkl@gmail.com>
2025-04-26 16:37:24 +1000

    push

--- front_end/next_app/.next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_globals_73c37791.css",
-  "static/chunks/node_modules_a1b1235b._.js",
-  "static/chunks/store_41207d0f._.js"
-],
-    source: "dynamic"
-});
