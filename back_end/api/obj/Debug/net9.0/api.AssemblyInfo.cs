a794788798e0d54b2614dfe4b45d705146aead01
Harken <antarcticbearhkl@gmail.com>
2025-04-26 13:42:58 +1000

    push

--- back_end/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ back_end/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88e797beb23271ae998539575832448de754dc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328336af6f1a063aa22e7bc2097d41fd7a4c7aa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
