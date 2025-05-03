fc3d7485b29d58d1ce174f413198c7a322816995
Harken <antarcticbearhkl@gmail.com>
2025-05-03 18:26:23 +1000

    fix

--- front_end/app/page.tsx
+++ front_end/app/page.tsx
@@ -96,6 +96,14 @@ export default function Home() {
           <span>⚙️</span>
         </div>
 
+        <button className="px-4 py-2 backdrop-blur-md bg-white/20 text-white rounded-full cursor-pointer shadow-sm"
+          onClick={() => {
+            document.documentElement.requestFullscreen()
+          }}
+        >
+          fc
+        </button>
+
       </div>
 
 
