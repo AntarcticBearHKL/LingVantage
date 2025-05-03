c43a432e43f11e9ce0f3dcaf951a198dbba7f033
Harken <antarcticbearhkl@gmail.com>
2025-05-03 17:34:21 +1000

    Update: Add Native Support

---
+++ native_app/windows/NativeApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::NativeApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::NativeApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
